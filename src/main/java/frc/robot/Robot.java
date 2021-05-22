d51d74813e2fa98e2f908e4f95d47c5f83967303
Paul Terrasi <paul.a.terrasi@gmail.com>
2021-05-22 13:55:19 -0400

    Run google formatter

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,153 +7,157 @@
 
 package frc.robot;
 
-import java.util.function.BooleanSupplier;
-import java.util.function.DoubleSupplier;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import frc.team88.swerve.SwerveController;
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
 
-    private enum ControllerMode {
-        kXBox,
-        kFrsky;
+  private enum ControllerMode {
+    kXBox,
+    kFrsky;
+  }
+
+  private final ControllerMode controllerMode = ControllerMode.kXBox;
+
+  private SwerveController swerve;
+
+  private Joystick gamepad;
+
+  private static final double MAX_SPEED = 14.7;
+  private static final double MAX_ROTATION = 90.;
+
+  private BooleanSupplier zeroButton;
+  private DoubleSupplier translationDirection;
+  private BooleanSupplier maintainDirection;
+  private DoubleSupplier translationSpeed;
+  private DoubleSupplier rotationVelocity;
+  private BooleanSupplier fieldCentricMode;
+
+  @Override
+  public void robotInit() {
+    this.swerve = new SwerveController("swerve.toml");
+    this.swerve.setGyroYaw(0);
+
+    this.gamepad = new Joystick(0);
+
+    switch (this.controllerMode) {
+      case kXBox:
+        this.zeroButton = () -> gamepad.getRawButton(4);
+        this.translationDirection =
+            () -> {
+              double x = gamepad.getRawAxis(0);
+              double y = -gamepad.getRawAxis(1);
+              return Math.toDegrees(Math.atan2(-x, y));
+            };
+        this.maintainDirection =
+            () -> {
+              double x = gamepad.getRawAxis(0);
+              double y = -gamepad.getRawAxis(1);
+              return Math.sqrt(x * x + y * y) < 0.25;
+            };
+        this.translationSpeed = () -> gamepad.getRawAxis(3) * MAX_SPEED;
+        this.rotationVelocity =
+            () -> deadbandExponential(gamepad.getRawAxis(4), 3, 0.075) * MAX_ROTATION;
+        this.fieldCentricMode = () -> !gamepad.getRawButton(6);
+        break;
+      case kFrsky:
+        this.zeroButton = () -> gamepad.getRawButton(4);
+        this.translationDirection = () -> 0;
+        this.maintainDirection = () -> true;
+        this.translationSpeed = () -> 0;
+        this.rotationVelocity = () -> 0;
+        this.fieldCentricMode = () -> true;
+        break;
     }
-    private final ControllerMode controllerMode = ControllerMode.kXBox;
+  }
 
-    private SwerveController swerve;
+  @Override
+  public void robotPeriodic() {
+    // Happens after mode periodic method
+    this.swerve.update();
+  }
 
-    private Joystick gamepad;
+  public void disabledInit() {}
 
-    private static final double MAX_SPEED = 14.7;
-    private static final double MAX_ROTATION = 90.;
+  @Override
+  public void disabledPeriodic() {
+    if (gamepad.getRawButton(7)) {
+      this.swerve.setCoast();
+    }
+    if (gamepad.getRawButton(8)) {
+      this.swerve.setBrake();
+    }
+    if (zeroButton.getAsBoolean()) {
+      this.swerve.setGyroYaw(0);
+    }
+  }
 
-    private BooleanSupplier zeroButton;
-    private DoubleSupplier translationDirection;
-    private BooleanSupplier maintainDirection;
-    private DoubleSupplier translationSpeed;
-    private DoubleSupplier rotationVelocity;
-    private BooleanSupplier fieldCentricMode;
+  @Override
+  public void autonomousInit() {
+    this.swerve.setBrake();
+  }
 
-    @Override
-    public void robotInit() {
-        this.swerve = new SwerveController("swerve.toml");
-        this.swerve.setGyroYaw(0);
+  @Override
+  public void autonomousPeriodic() {
+    this.swerve.holdDirection();
+  }
 
-        this.gamepad = new Joystick(0);
+  @Override
+  public void teleopInit() {
+    this.swerve.setBrake();
+  }
 
-        switch (this.controllerMode) {
-            case kXBox:
-                this.zeroButton = () -> gamepad.getRawButton(4);
-                this.translationDirection = () -> {
-                    double x = gamepad.getRawAxis(0);
-                    double y = -gamepad.getRawAxis(1);
-                    return Math.toDegrees(Math.atan2(-x, y));
-                };
-                this.maintainDirection = () -> {
-                    double x = gamepad.getRawAxis(0);
-                    double y = -gamepad.getRawAxis(1);
-                    return Math.sqrt(x*x + y*y) < 0.25;
-                };
-                this.translationSpeed = () -> gamepad.getRawAxis(3) * MAX_SPEED;
-                this.rotationVelocity = () -> deadbandExponential(gamepad.getRawAxis(4), 3, 0.075) * MAX_ROTATION;
-                this.fieldCentricMode = () -> !gamepad.getRawButton(6);
-                break;
-            case kFrsky:
-                this.zeroButton = () -> gamepad.getRawButton(4);
-                this.translationDirection = () -> 0;
-                this.maintainDirection = () -> true;
-                this.translationSpeed = () -> 0;
-                this.rotationVelocity = () -> 0;
-                this.fieldCentricMode = () -> true;
-                break;
-        }
+  @Override
+  public void teleopPeriodic() {
+    this.swerve.setVelocity(
+        this.translationSpeed.getAsDouble(), this.rotationVelocity.getAsDouble());
+
+    if (!this.maintainDirection.getAsBoolean()) {
+      this.swerve.setTranslationDirection(
+          this.translationDirection.getAsDouble(), this.fieldCentricMode.getAsBoolean());
+    } else if (this.translationSpeed.getAsDouble() < 0.1
+        && Math.abs(this.rotationVelocity.getAsDouble()) < 0.1) {
+      this.swerve.holdDirection();
+    }
+  }
+
+  @Override
+  public void testInit() {
+    this.swerve.setBrake();
+  }
+
+  @Override
+  public void testPeriodic() {
+    this.swerve.holdDirection();
+  }
+
+  @Override
+  public void simulationInit() {}
+
+  private static double signedPow(double base, int exp) {
+    double value = 0;
+
+    if (base < 0 && exp % 2 == 0) {
+      value = -Math.pow(base, exp);
+    } else {
+      value = Math.pow(base, exp);
     }
 
-    @Override
-    public void robotPeriodic() {
-        // Happens after mode periodic method
-        this.swerve.update();
-    }
+    return value;
+  }
 
-    public void disabledInit() {
-    }
-
-    @Override
-    public void disabledPeriodic() {
-        if (gamepad.getRawButton(7)) {
-            this.swerve.setCoast();
-        }
-        if (gamepad.getRawButton(8)) {
-            this.swerve.setBrake();
-        }
-        if (zeroButton.getAsBoolean()) {
-            this.swerve.setGyroYaw(0);
-        }
-    }
-
-    @Override
-    public void autonomousInit() {
-        this.swerve.setBrake();
-    }
-
-    @Override
-    public void autonomousPeriodic() {
-        this.swerve.holdDirection();
-    }
-
-    @Override
-    public void teleopInit() {
-        this.swerve.setBrake();
-    }
-
-    @Override
-    public void teleopPeriodic() {
-        this.swerve.setVelocity(this.translationSpeed.getAsDouble(), this.rotationVelocity.getAsDouble());
-
-        if (!this.maintainDirection.getAsBoolean()) {
-            this.swerve.setTranslationDirection(this.translationDirection.getAsDouble(), this.fieldCentricMode.getAsBoolean());
-        } else if (this.translationSpeed.getAsDouble() < 0.1 && Math.abs(this.rotationVelocity.getAsDouble()) < 0.1) {
-            this.swerve.holdDirection();
-        }
-    }
-
-    @Override
-    public void testInit() {
-        this.swerve.setBrake();
-    }
-
-    @Override
-    public void testPeriodic() {
-        this.swerve.holdDirection();
-    }
-
-    @Override
-    public void simulationInit() {
-    }
-
-    private static double signedPow(double base, int exp){
-        double value = 0;
-        
-        if(base < 0 && exp%2==0){
-            value = -Math.pow(base,exp);
-        }
-        else{
-            value = Math.pow(base,exp);
-        }
-
-        return value;
-    }
-    
-    private static double deadbandExponential(double spd, int exp, double deadband) {
-        return Math.abs(spd)<deadband ? 0 : signedPow((spd - deadband*Math.signum(spd)) / (1 - deadband), exp);
-    }
+  private static double deadbandExponential(double spd, int exp, double deadband) {
+    return Math.abs(spd) < deadband
+        ? 0
+        : signedPow((spd - deadband * Math.signum(spd)) / (1 - deadband), exp);
+  }
 }
